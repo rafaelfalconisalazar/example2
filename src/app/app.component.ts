76ad79e8ec4fb79d1ac99115ec672e4931921430
Rafael <r.falconi9030@gmail.com>
2019-11-30 08:20:33 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'example2';
+  title2 = "rafael";
+  arreglo=["a","adadf","adasdfasdsdf","adadsf"];
+  imagenes=["https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/2018_Bogot%C3%A1_edificio_UGI.jpg/413px-2018_Bogot%C3%A1_edificio_UGI.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Edificio_Duque_de_Lerma_Valladolid.jpg/413px-Edificio_Duque_de_Lerma_Valladolid.jpg"]
+}
